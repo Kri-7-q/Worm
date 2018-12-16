9d3a06420a3554227d7c100122abd7ceb35e6341
Christian Weiß <weiss-christian@hotmail.de>
2018-12-16 11:16:06 +0100

    Worm model
    Implemented the model for the worm.
    Controler can modify the worm model by setHead() method.
    Model roles modified.

--- Models/wormmodel.cpp
+++ Models/wormmodel.cpp
@@ -1,30 +1,66 @@
 #include "wormmodel.h"
 
+// Constructor
 WormModel::WormModel(QObject *parent)
     : QAbstractListModel(parent)
+    , m_data(10)
 {
+    m_roles.insert(PointX, QString("pointX").toLatin1());
+    m_roles.insert(PointY, QString("pointY").toLatin1());
+    m_roles.insert(Point, QString("point").toLatin1());
+
+    // Just for testing MUST be removed later
+    QPoint pt(200, 200);
+    while (m_data.capacity() > m_data.size()) {
+        setHead(pt);
+        pt.setY( pt.y()+8 );
+    }
 }
 
-QVariant WormModel::headerData(int section, Qt::Orientation orientation, int role) const
-{
-    // FIXME: Implement me!
-}
-
+// Override
+// QAbstractListModel:  rowCount()
 int WormModel::rowCount(const QModelIndex &parent) const
 {
-    // For list models only the root node (an invalid parent) should return the list's size. For all
-    // other (valid) parents, rowCount() should return 0 so that it does not become a tree model.
-    if (parent.isValid())
-        return 0;
-
-    // FIXME: Implement me!
+    Q_UNUSED(parent)
+    return static_cast<int>(m_data.size());
 }
 
+// Override
+// QAbstractListModel:  data()
 QVariant WormModel::data(const QModelIndex &index, int role) const
 {
-    if (!index.isValid())
-        return QVariant();
+    const QPoint& pt = m_data.at(static_cast<unsigned>(index.row()));
+    QVariant val = QVariant();
+    switch (role) {
+    case PointX:
+        val.setValue( pt.x() );
+        break;
+    case PointY:
+        val.setValue( pt.y() );
+        break;
+    case Point:
+        val.setValue(pt);
+        break;
+    }
 
-    // FIXME: Implement me!
-    return QVariant();
+    return val;
+}
+
+// Override
+// QAbstractListModel:  roleNames()
+QHash<int, QByteArray> WormModel::roleNames() const
+{
+    return m_roles;
+}
+
+// Set a new head position.
+void WormModel::setHead(const QPoint &pt)
+{
+//    beginInsertRows(index(0), 0 , 0);
+    m_data << pt;
+//    endInsertRows();
+    int last = static_cast<int>( m_data.size() - 1 );
+//    beginRemoveRows(index(last), last, last);
+//    endRemoveRows();
+    dataChanged(index(0), index(last), QVector<int>() << Point << PointX << PointY);
 }
--- Models/wormmodel.h
+++ Models/wormmodel.h
@@ -1,24 +1,30 @@
 #ifndef WORMMODEL_H
 #define WORMMODEL_H
 
+#include "Utility/ringmem.h"
 #include <QAbstractListModel>
+#include <QPoint>
+
 
 class WormModel : public QAbstractListModel
 {
     Q_OBJECT
 
 public:
+    enum Role { PointX=Qt::UserRole, PointY, Point };
+
     explicit WormModel(QObject *parent = nullptr);
 
-    // Header:
-    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
-
-    // Basic functionality:
+    // QAbstractListModel interface
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+    void setHead(const QPoint& pt);
 
 private:
+    RingMem<QPoint> m_data;
+    QHash<int, QByteArray> m_roles;
 };
 
 #endif // WORMMODEL_H
